bb3fd0a0c8a7990111a488638b31129a63568202
Hao Kong <2848914089@qq.com>
2023-04-27 17:52:36 +0800

    notes

---
+++ test_4_26.c
@@ -0,0 +1,117 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+//int main()
+//{
+//	int i = 0;
+//	for (i = 0; i < 100; i++)
+//	{
+//		printf("%d ", i);
+//	}
+//	system("pause");
+//	return 0;
+//}
+
+//int main()
+//{
+//	int i = 0;
+//	int arr[10] = { 1,2,3,4,5,6,7,8,9,10 };
+//	for (; i <= 12; i++)
+//	{
+//		printf("hehe\n");
+//		arr[i] = 0;
+//	}
+//	system("pause");
+//	return 0;
+//}
+
+
+//F9断点 与F5连用
+//int main()
+//{
+//	int i = 0;
+//	for (i = 0; i < 100; i++)
+//	{
+//		printf("%d ", i);
+//	}
+//	for (i = 0; i < 100; i++)
+//	{
+//		printf("%d ", 10 - i);
+//	}
+//	return 0;
+//}
+
+
+//int Add(int x, int y)
+//{
+//	return x + y;
+//}
+//int main()
+//{
+//	printf("hehe\n");
+//	int a = 10;
+//	int b = 20;
+//	int c = Add(a, b);
+//	return 0;
+//}
+
+//int main()
+//{
+//	{
+//		int tmp = 0;
+//		printf("tmp=%d\n", tmp);
+//	}
+//	int arr[10] = { 0 };
+//	int i = 0;
+//	for (; i < 10; i++)
+//	{
+//		arr[i] = i;
+//	}
+//	return 0;
+//}
+
+
+//void test2()
+//{
+//	printf("hehe\n");
+//}
+//
+//void test1()
+//{
+//	test2();
+//}
+//
+//void test()
+//{
+//	test1();
+//}
+//int main()
+//{
+//	test();
+//	return 0;
+//}
+
+//int main()
+//{
+//	int i = 0;
+//	int ret = 1;
+//	int sum = 0;
+//	int n = 0;
+//	scanf("%d", &n);
+//	for (i = 1; i <= n; i++)
+//	{
+//		int j = 0;
+//		ret = 1;
+//		for (j = 1; j <= i; j++)
+//		{
+//			ret *= j;
+//		}
+//		sum += ret;
+//	}
+//	printf("%d\n", sum);
+//	return 0;
+//}
+
